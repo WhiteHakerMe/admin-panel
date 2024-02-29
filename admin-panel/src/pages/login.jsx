6f8754534a0076769e5d855debd8b70160cb9cea
haker <140898334+WhiteHakerMe@users.noreply.github.com>
2024-02-29 18:07:48 +0500

    main

--- admin-panel/src/pages/login.jsx
+++ admin-panel/src/pages/login.jsx
@@ -1,43 +1,66 @@
-import {  Button, Form, Input } from 'antd'
-import React from 'react'
+import { Button, Checkbox, Form, Input } from 'antd'
+import React, { useContext } from 'react'
+import { MainContext } from '../context/mainContext';
 
 const Login = () => {
+
+  const { user, setUser } = useContext(MainContext)
+  const onFinish = (values) => {
+    if (values.username == "ilyosbek" && values.Password == "ilyosbek") {
+      setUser(true)
+      console.log(user);
+    }
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+
   return (
     <section className='w-full h-[100vh] bg-gray-200 flex items-center justify-center'>
-       <Form onSubmit={handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />,
-          )}
+      <Form
+        className='bg-white p-5 w-1/3'
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
         </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />,
-          )}
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
         </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(<Checkbox>Remember me</Checkbox>)}
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item >
+          <Button className='bg-blue-500 ml-auto block text-white px-10' type="default" htmlType="submit" size='large'>
+            Submit
           </Button>
-          Or <a href="">register now!</a>
         </Form.Item>
       </Form>
     </section>
